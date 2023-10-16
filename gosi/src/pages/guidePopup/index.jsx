5ebeb9fc9f486ffdceca882e535d79d0d16a4154
kmingi93 <kmingi93@gmail.com>
2023-10-16 08:45:50 +0000

    수강신청 작업

--- gosi/src/pages/guidePopup/index.jsx
+++ gosi/src/pages/guidePopup/index.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import "scss/guidePopup.scss";
 export default function Index() {
+  const [tabActive, SetTabActive] = useState(0);
   return (
     <>
-      <h1>guidePopup</h1>
+      <div className="pop_apply_guide">
+        <h2 className="pop_tit">
+          <img
+            src="https://egosi.hackers.com/images/lecture/guide_tit.jpg"
+            alt=""
+          />
+        </h2>
+        <div className="tab_area">
+          <ul className="pop_tab">
+            <li
+              className={tabActive === 0 ? "active" : null}
+              onClick={() => {
+                SetTabActive(0);
+              }}
+            >
+              <button type="button">단과 수강신청</button>
+            </li>
+            <li
+              className={tabActive === 1 ? "active" : null}
+              onClick={() => {
+                SetTabActive(1);
+              }}
+            >
+              <button type="button">패스 수강신청</button>
+            </li>
+          </ul>
+          <div className={tabActive === 0 ? "tabcon active" : "tabcon"}>0</div>
+          <div className={tabActive === 1 ? "tabcon active" : "tabcon"}>1</div>
+        </div>
+      </div>
     </>
   );
 }
